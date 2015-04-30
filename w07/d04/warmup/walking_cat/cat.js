49f5bca4de0208912aa91b62780065057c912c43
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-04-30 10:07:17 -0400

    walking cat exercise

--- w07/d04/warmup/walking_cat/cat.js
+++ w07/d04/warmup/walking_cat/cat.js
@@ -1,5 +1,29 @@
-walkingCat();
+var catImage = document.querySelector("img")
+var animate
+// var screenWidth = window.innerWidth;
+
+function init(){
+  var catImage = document.querySelector("img")
+  catImage.style.position='relative'
+  catImage.style.left='0px';
+}
 
 function walkingCat() {
-
+  catImage.style.left = parseInt(catImage.style.left) + 10 + 'px';
+  animate = setTimeout(walkingCat, 100);
 }
+
+catImage.addEventListener("click", function(event){
+  walkingCat();
+})
+
+function stop() {
+  clearTimeout(walkingCat)
+  catImage.style.left = '0px'
+}
+
+window.addEventListener("click", function(event){
+  stop(animate);
+})
+
+window.onload =init;
