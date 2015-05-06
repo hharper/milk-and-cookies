6e759c6969739f1fe5848e7716c5e5e8f0e8c361
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-05-06 12:26:25 -0400

    accessed weather underground API with ajax

--- w08/d02b_ajax/exercises/ajax_weather_underground/script.js
+++ w08/d02b_ajax/exercises/ajax_weather_underground/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-  alert("test")
+  var wUnderGroundurl = "http://api.wunderground.com/api/0012f7aa329ab2f4/conditions/q/CA/San_Francisco.json"
+  $.ajax({
+    url: wUnderGroundurl,
+    type: "GET",
+    dataType: "json"
+  }).done(function(response){
+    console.log("temperature " + response.current_observation.temp_f)
+    console.log("location " + response.current_observation.display_location.full)
+    console.log("wind gust MPH: " + response.current_observation.wind_gust_mph)
+    console.log("wind gust KPH: " + response.current_observation.wind_gust_kph)
+  }).fail(function(){
+    console.log("ajax request unsuccessful")
+  }).always(function(){
+    console.log("this always happens!")
+  })
 })
